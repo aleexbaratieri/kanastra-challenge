f48385497ff171a8e755e928542ef6e9ed88f9f4
Alex Baratieri <alex.baratieri@cashin.com.br>
2024-05-07 12:07:20 -0300

    feat: root config

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import * as Components from './components'
+import { ListBillingProvider } from './contexts/billings'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Components.NoMatch />
-      {/* Your Job here */}
+      <ListBillingProvider>
+        <Components.App/>
+      </ListBillingProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
